34c42d9baef8cee47ae2766f9646eb6a6635aefa
Anantha Padmanabhan CB <cbpadman@cisco.com>
2015-10-20 04:15:46 +0530

    Added more implementation template

--- agent/agent.py
+++ agent/agent.py
@@ -2,11 +2,13 @@ from config_reader import ConfigReader
 import logging
 from monitor import Monitor
 from log_manager import LogManager
-
+import docker
+import threading
 logger = logging.getLogger("agent-logger")
 
 
 class Agent(object):
+    """Agent runs on each host collecting statistics from the host and the containers"""
     def __init__(self):
         log_mgr = LogManager()
         config_reader = ConfigReader("config.txt")
@@ -14,13 +16,26 @@ class Agent(object):
         logger.info("Loaded configurations")
         driver = config['monitoring_driver']
         file_name, cls_name = driver.strip().split(".")
-
         module = __import__(file_name)
         driver_class = getattr(module, cls_name)
         self.monitor = Monitor(driver_class)
+        self.stats = {}
+        self.containers = []
 
+    def get_all_stats(self):
+        self.stats['host'] = self.monitor.collect_host_stats()
+        self.stats['container'] = self.monitor.collect_container_stats()
 
-if __name__ == "__main__":
-    agent = Agent()
+    def get_all_errors(self):
+        self.stats['error'] = self.monitor.collect_errors()
 
-    logger.info("Home Directory : %s" % agent.monitor.collect_host_stats())
+    def list_all_containers(self):
+        #todo: Needs to be tested
+        threading.Timer(5.0, self.list_all_containers).start()
+        client = docker.Client(base_url='unix://var/run/docker.sock', version="1.20")
+        self.containers = client.containers()
+
+# if __name__ == "__main__":
+#    agent = Agent()
+
+#    logger.info("Home Directory : %s" % agent.monitor.collect_host_stats())
--- agent/pcp_monitor.py
+++ agent/pcp_monitor.py
@@ -13,13 +13,35 @@ class PcpMonitor(Monitor):
         logger.info("Using PCP to monitor Docker")
 
     def collect_host_stats(self):
-        return os.getenv('HOME')
 
-    def collect_container_stats(self):
-        pass
+        self.host_stats['memory'] = self._get_mem_stats()
+        self.host_stats['cpu'] = self._get_cpu_stats()
+        self.host_stats['disk'] = self._get_disk_stats()
+        self.host_stats['network'] = self._get_nw_stats()
+
+        return self.host_stats
+
+    def collect_container_stats(self, container_id):
+        self.container_stats[container_id] = self._get_container_stats(container_id)
+        return self.container_stats
 
     def collect_errors(self):
         pass
 
     def stream_logs(self):
-        pass
+        return {}
+
+    def _get_mem_stats(self):
+        return {}
+
+    def _get_cpu_stats(self):
+        return {}
+
+    def _get_disk_stats(self):
+        return {}
+
+    def _get_nw_stats(self):
+        return {}
+
+    def _get_container_stats(self, id):
+        return {}
